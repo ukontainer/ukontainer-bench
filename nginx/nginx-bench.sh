2bd5fbd97fd3ae8a0ac57febae0057d9b9be1b0c
Hajime Tazaki <thehajime@gmail.com>
2021-05-21 17:00:21 +0900

    update nginx for mino

--- nginx/nginx-bench.sh
+++ nginx/nginx-bench.sh
@@ -3,27 +3,36 @@
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 #source "$SCRIPT_DIR/netperf-common.sh"
 
-DEST_ADDR="3.3.3.2"
-SELF_ADDR="3.3.3.3"
-HOST_ADDR="3.3.3.5"
-export FIXED_ADDRESS="$SELF_ADDR"
-export FIXED_MASK=24
+DEST_ADDR="10.0.39.2"
+SELF_ADDR="172.17.0.39"
+HOST_ADDR="10.0.39.1"
 TRIALS=1
+PKG_SIZES="1024 4096 16384 65536 262144"
 
 LKLMUSL_NGINX="$HOME/gitworks/rumprun-packages/"
-NATIVE_WRK="work/wrk/"
+LKL_HIJACK_ROOT="/home/tazaki/work/lkl-linux"
+NGINX_BIN="/home/tazaki/work/nginx-ktls/objs/nginx"
+NGINX_NATIVE_ROOT="/home/tazaki/work/nginx-ktls/root"
 export PATH=~/bin:~/gitworks/frankenlibc/rump/bin:${PATH}
+
+WRK_NUM_CONN=64
+WRK_NUM_THREAD=8
+WRK_CMD="~/work/wrk/wrk -t $WRK_NUM_THREAD -c $WRK_NUM_CONN --latency"
 
 OUTPUT="$(date "+%Y-%m-%d")"
 
 main() {
   mkdir -p "$OUTPUT"
   rm -f "$OUTPUT"/nginx-*.dat
+  exec > >(tee "$OUTPUT/$(basename $0).log") 2>&1
 
   # for nginx/wrk tests
   for num in $(seq 1 "$TRIALS"); do
-    for size in 64 128 256 512 1024 1500 2048; do
+    for size in $PKG_SIZES; do
       nginx::run wrk $num $size
+      nginx::run wrk $num $size https
+      nginx::run wrk $num $size https-ktls
+      nginx::run wrk $num $size https-ktls-sendfile
     done
   done
 
@@ -31,9 +40,53 @@ main() {
 }
 
 nginx::run() {
-  nginx::lkl "$@"
+  #nginx::lkl "$@"
+  nginx::lkl-hijack "$@"
   nginx::native "$@"
-  nginx::docker "$@"
+  #nginx::docker "$@"
+}
+
+
+nginx::lkl-hijack() {
+(
+  local test=$1
+  local num=$2
+  local ex_arg=$3
+  local tls=$4
+  local conf=/conf/nginx.conf
+  if [ "$tls" == "https" ] ; then
+      # non kTLS
+      local wrk_args="-Z AES128-GCM-SHA256 https://$SELF_ADDR/${ex_arg}b.img"
+  elif [ "$tls" == "https-ktls" ] ; then
+      # kTLS
+      local wrk_args="-Z AES128-GCM-SHA256 https://$SELF_ADDR/${ex_arg}b.img"
+      conf=/conf/nginx-ktls.conf
+  elif [ "$tls" == "https-ktls-sendfile" ] ; then
+      # kTLS
+      local wrk_args="-Z AES128-GCM-SHA256 https://$SELF_ADDR/sf/${ex_arg}b.img"
+      conf=/conf/nginx-ktls.conf
+  else
+      local wrk_args="http://$SELF_ADDR/${ex_arg}b.img"
+      tls="http"
+  fi
+
+  #setup_lkl
+
+  echo "$(tput bold)== lkl-hijack ($test-$num-$tls-p${ex_arg}) ==$(tput sgr0)"
+  killall -9 nginx
+  MINO_EXCLUDE="$LKL_HIJACK_ROOT/,./,/dev" \
+	      LKL_HIJACK_CONFIG_FILE=./lkl-hijack-ktls.json \
+	      taskset -c 0 \
+	      $LKL_HIJACK_ROOT/tools/lkl/bin/lkl-hijack.sh $NGINX_BIN -p / -c $conf \
+     &
+  sleep 3
+
+  ssh "$DEST_ADDR" "$WRK_CMD" "$wrk_args" \
+    2>&1 | tee -a "$OUTPUT/nginx-$test-$tls-lkl-hijack-$num.dat"
+
+  kill $!
+  wait $! 2>/dev/null
+)
 }
 
 nginx::lkl() {
@@ -45,16 +98,16 @@ nginx::lkl() {
 
   #setup_lkl
 
-  echo "$(tput bold)== lkl ($test-$num-p${ex_arg}) ==$(tput sgr0)"
+  echo "$(tput bold)== lkl ($test-$num-$tls-p${ex_arg}) ==$(tput sgr0)"
   sudo killall -9 nginx
   sudo rexec "$LKLMUSL_NGINX/nginx/bin/nginx" \
     "$LKLMUSL_NGINX/nginx/images/data.iso" tap:tap0 config:../lkl.json \
     | grep -v fallback &
-  sleep 1
+  sleep 3
   sudo ifconfig tap0 up; sudo ifconfig bridge1 addm tap0
 
   ssh -t "$DEST_ADDR" sudo arp -d "$FIXED_ADDRESS"
-  ssh "$DEST_ADDR" "$NATIVE_WRK/wrk" "$wrk_args" \
+  ssh "$DEST_ADDR" "$WRK_CMD" "$wrk_args" \
     2>&1 | tee -a "$OUTPUT/nginx-$test-lkl-$num.dat"
 
   sudo kill $!
@@ -67,14 +120,31 @@ nginx::native() {
   local test=$1
   local num=$2
   local ex_arg=$3
-  local wrk_args="http://${HOST_ADDR}:8080/${ex_arg}b.img"
+  local tls=$4
+  local conf=$NGINX_NATIVE_ROOT/conf/nginx.conf
+  if [ "$tls" == "https" ] ; then
+      # non kTLS case
+      local wrk_args="-Z AES128-GCM-SHA256 https://${HOST_ADDR}:8080/${ex_arg}b.img"
+  elif [ "$tls" == "https-ktls" ] ; then
+      # kTLS
+      local wrk_args="-Z AES128-GCM-SHA256 https://${HOST_ADDR}:8080/${ex_arg}b.img"
+      conf=$NGINX_NATIVE_ROOT/conf/nginx-ktls.conf
+  elif [ "$tls" == "https-ktls-sendfile" ] ; then
+      # kTLS
+      local wrk_args="-Z AES128-GCM-SHA256 https://${HOST_ADDR}:8080/sf/${ex_arg}b.img"
+      conf=$NGINX_NATIVE_ROOT/conf/nginx-ktls.conf
+  else
+      local wrk_args="http://${HOST_ADDR}:8081/${ex_arg}b.img"
+      tls="http"
+  fi
 
-  echo "$(tput bold)== native ($test-$num-p${ex_arg})  ==$(tput sgr0)"
-  nginx -s stop
-  nginx
-  ssh "$DEST_ADDR" "$NATIVE_WRK/wrk" "$wrk_args" \
-    2>&1 | tee -a "$OUTPUT/nginx-$test-native-$num.dat"
-  nginx -s stop
+
+  echo "$(tput bold)== native ($test-$num-$tls-p${ex_arg})  ==$(tput sgr0)"
+  killall -9 nginx
+  taskset -c 0 $NGINX_BIN -p $NGINX_NATIVE_ROOT -c $conf &
+  ssh "$DEST_ADDR" "$WRK_CMD" "$wrk_args" \
+    2>&1 | tee -a "$OUTPUT/nginx-$test-$tls-native-$num.dat"
+  killall -9 nginx
 )
 }
 
@@ -91,7 +161,7 @@ nginx::docker() {
   nginx-test nginx &
 
   sleep 5
-  ssh "$DEST_ADDR" "$NATIVE_WRK/wrk" "$wrk_args" \
+  ssh "$DEST_ADDR" "$WRK_CMD" "$wrk_args" \
     2>&1 | tee -a "$OUTPUT/nginx-$test-docker-$num.dat"
 
   kill $!
--- nginx/nginx-plot.sh
+++ nginx/nginx-plot.sh
@@ -1,29 +1,66 @@
 
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+source "$SCRIPT_DIR/../common.sh"
+
 OUTPUT=$1
 mkdir -p ${OUTPUT}/out
 
-PKG_SIZES="64 128 256 512 1024 1500 2048"
-
+PKG_SIZES="1024 4096 16384 65536 262144"
 
 # parse outputs
 
 # thpt (req/sec)
-grep -E -h Req/Sec  ${OUTPUT}/nginx*-lkl-[0-9].*  \
- | awk '{print $2 " " $3}' | sed "s/k/ 1000/g" | awk '{print $1*$2*2 " " $3}' \
- > ${OUTPUT}/nginx-lkl-thpt.dat
-grep -E -h Req/Sec  ${OUTPUT}/nginx*-native-[0-9].*  \
- | awk '{print $2 " " $3}' | sed "s/k/ 1000/g" | awk '{print $1*$2*2 " " $3}' \
- > ${OUTPUT}/nginx-native-thpt.dat
-grep -E -h Req/Sec  ${OUTPUT}/nginx*-docker-[0-9].*  \
- | awk '{print $2 " " $3}' | sed "s/k/ 1000/g" | awk '{print $1*$2*2 " " $3}' \
- > ${OUTPUT}/nginx-docker-thpt.dat
+for tls in http https https-ktls https-ktls-sendfile; do
+grep -E -h Req/Sec  ${OUTPUT}/nginx*-$tls-lkl-[0-9].*  \
+    | awk '{print $2 " " $3}' | sed "s/k/K/g" | numfmt --from=si --field=1,2 | \
+    awk '{print $1 " " $2}' \
+ > ${OUTPUT}/nginx-$tls-lkl-thpt.dat
+grep -E -h Req/Sec  ${OUTPUT}/nginx*-$tls-lkl-hijack-[0-9].*  \
+    | awk '{print $2 " " $3}' | sed "s/k/K/g" | numfmt --from=si --field=1,2 | \
+    awk '{print $1 " " $2}' \
+ > ${OUTPUT}/nginx-$tls-lkl-hijack-thpt.dat
+grep -E -h Requests/sec  ${OUTPUT}/nginx*-$tls-lkl-hijack-[0-9].*  \
+    | awk '{print $2}'  \
+ > ${OUTPUT}/nginx-$tls-lkl-hijack-thpt.dat
+grep -E -h Req/Sec  ${OUTPUT}/nginx*-$tls-native-[0-9].*  \
+    | awk '{print $2 " " $3}' | sed "s/k/K/g" | numfmt --from=si --field=1,2 | \
+    awk '{print $1 " " $2}' \
+ > ${OUTPUT}/nginx-$tls-native-thpt.dat
+grep -E -h Requests/sec  ${OUTPUT}/nginx*-$tls-native-[0-9].*  \
+    | awk '{print $2}'  \
+ > ${OUTPUT}/nginx-$tls-native-thpt.dat
+grep -E -h Req/Sec  ${OUTPUT}/nginx*-$tls-docker-[0-9].*  \
+    | awk '{print $2 " " $3}' | sed "s/k/K/g" | numfmt --from=si --field=1,2 | \
+    awk '{print $1 " " $2}' \
+ > ${OUTPUT}/nginx-$tls-docker-thpt.dat
 # latency
-grep -E -h Latency ${OUTPUT}/nginx*-lkl-[0-9].*  \
- | awk '{print $2 " " $3}' | sed "s/ms/ 1000/g" | sed "s/us/ 1/g" | awk '{print $1*$2 " " $3*$4}'  > ${OUTPUT}/nginx-lkl.dat
-grep -E -h Latency ${OUTPUT}/nginx*-native-[0-9]* \
- | awk '{print $2 " " $3}' | sed "s/ms/ 1000/g" | sed "s/us/ 1/g" | awk '{print $1*$2 " " $3*$4}'  > ${OUTPUT}/nginx-native.dat
-grep -E -h Latency ${OUTPUT}/nginx*-docker-[0-9]* \
- | awk '{print $2 " " $3}' | sed "s/ms/ 1000/g" | sed "s/us/ 1/g" | awk '{print $1*$2 " " $3*$4}'  > ${OUTPUT}/nginx-docker.dat
+grep -E -h "Latency.*%" ${OUTPUT}/nginx*-$tls-lkl-[0-9].*  \
+ | awk '{print $2 " " $3}' | sed "s/ms/ 1000/g" | sed "s/us/ 1/g" | awk '{print $1*$2 " " $3*$4}'  > ${OUTPUT}/nginx-$tls-lkl.dat
+grep -E -h "Latency.*%" ${OUTPUT}/nginx*-$tls-lkl-hijack-[0-9].*  \
+ | awk '{print $2 " " $3}' | sed "s/ms/ 1000/g" | sed "s/us/ 1/g" | awk '{print $1*$2 " " $3*$4}'  > ${OUTPUT}/nginx-$tls-lkl-hijack.dat
+grep -E -h "Latency.*%" ${OUTPUT}/nginx*-$tls-native-[0-9]* \
+ | awk '{print $2 " " $3}' | sed "s/ms/ 1000/g" | sed "s/us/ 1/g" | awk '{print $1*$2 " " $3*$4}'  > ${OUTPUT}/nginx-$tls-native.dat
+grep -E -h "Latency.*%" ${OUTPUT}/nginx*-$tls-docker-[0-9]* \
+ | awk '{print $2 " " $3}' | sed "s/ms/ 1000/g" | sed "s/us/ 1/g" | awk '{print $1*$2 " " $3*$4}'  > ${OUTPUT}/nginx-$tls-docker.dat
+
+done
+
+PLOT_STRING_THPT="plot \\"
+
+###if [ ! -s ${OUTPUT}/nginx-docker-thpt.dat ] ; then
+###   PLOT_STRING_THPT=$PLOT_STRING_THPT' \'${OUTPUT}/nginx-docker-thpt.dat\' usi (\$0-0.3):(\$1/1000):(\$2/1000) w boxerr lt 1 lc rgb green fill pattern 2 title "docker(mac)" , '
+###fi
+###if [ ! -s ${OUTPUT}/nginx-lkl-thpt.dat ] ; then
+###   PLOT_STRING_THPT=$PLOT_STRING_THPT' \'${OUTPUT}/nginx-lkl-thpt.dat\' usi (\$0-0):(\$1/1000):(\$2/1000) w boxerr lt 1 lc rgb cyan fill pattern 4 title "lkl", \'
+###fi
+###if [ ! -s ${OUTPUT}/nginx-lkl-hijack-thpt.dat ] ; then
+###   PLOT_STRING_THPT=$PLOT_STRING_THPT' \'${OUTPUT}/nginx-lkl-hijack-thpt.dat\' usi (\$0-0):(\$1/1000):(\$2/1000) w boxerr lt 1 lc rgb blue fill pattern 6 title "lkl-hijack", \'
+###fi
+###if [ ! -s ${OUTPUT}/nginx-native-thpt.dat ] ; then
+###   PLOT_STRING_THPT=$PLOT_STRING_THPT' \'${OUTPUT}/nginx-native-thpt.dat\' usi (\$0+0.3):(\$1/1000):(\$2/1000) w boxerr fill pattern 0 lt 1 lc rgb red title "native(mac)"'
+###fi
+
+
 
 gnuplot  << EndGNUPLOT
 set terminal postscript eps lw 3 "Helvetica" 24
@@ -32,25 +69,30 @@ set pointsize 2
 set xzeroaxis
 set grid ytics
 
-set boxwidth 0.3
+set boxwidth 0.1
 set style fill pattern
 set key top righ
 set size 1.0,0.7
 
 # trans/sec
 set ylabel "Throughput (KReq/sec)"
-set ytics 5
-set yrange [0:20]
-set xtics ("64" 0, "128" 1, "256" 2, "512" 3, "1024" 4, "1500" 5, "2048" 6)
+set yrange [0:200]
+set ytics 50
+set xtics ("1k" 0, "4k" 1, "16k" 2, "64k" 3, "256k" 4)
 set xlabel "File size (bytes)"
-set xrange [-1:7]
+set xrange [-1:5]
 set terminal postscript eps lw 3 "Helvetica" 24
 set output "${OUTPUT}/out/nginx-wrk-thpt.eps"
 
 plot \
-   '${OUTPUT}/nginx-docker-thpt.dat' usi (\$0-0.3):(\$1/1000):(\$2/1000) w boxerr lt 1 lc rgb "green" fill pattern 2 title "docker(mac)" ,\
-   '${OUTPUT}/nginx-lkl-thpt.dat' usi (\$0-0):(\$1/1000):(\$2/1000) w boxerr lt 1 lc rgb "cyan" fill pattern 4 title "lkl", \
-   '${OUTPUT}/nginx-native-thpt.dat' usi (\$0+0.3):(\$1/1000):(\$2/1000) w boxerr fill pattern 0 lt 1 lc rgb "red" title "native(mac)" 
+   '${OUTPUT}/nginx-http-lkl-hijack-thpt.dat' usi (\$0-0.35):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_RUNU title "lkl", \
+   '${OUTPUT}/nginx-https-lkl-hijack-thpt.dat' usi (\$0-0.25):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_RUNU fill patter 1 title "lkl(https)", \
+   '${OUTPUT}/nginx-https-ktls-lkl-hijack-thpt.dat' usi (\$0-0.15):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_RUNU fill patter 1 title "lkl(https-ktls)", \
+   '${OUTPUT}/nginx-https-ktls-sendfile-lkl-hijack-thpt.dat' usi (\$0-0.05):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_RUNU fill patter 1 title "lkl(https-ktls-sendfile)", \
+   '${OUTPUT}/nginx-http-native-thpt.dat' usi (\$0+0.05):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_NATIVE title "native", \
+   '${OUTPUT}/nginx-https-native-thpt.dat' usi (\$0+0.15):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_NATIVE fill patter 1 title "native(https)", \
+   '${OUTPUT}/nginx-https-ktls-native-thpt.dat' usi (\$0+0.25):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_NATIVE fill patter 1 title "native(https-ktls)", \
+   '${OUTPUT}/nginx-https-ktls-sendfile-native-thpt.dat' usi (\$0+0.35):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_NATIVE fill patter 1 title "native(https-ktls-sendfile)"
 
 set terminal png lw 3 14 crop
 set output "${OUTPUT}/out/nginx-wrk-thpt.png"
@@ -58,18 +100,23 @@ replot
 
 # latency
 set ylabel "Latency (msec)"
-set ytics 1
-set yrange [0:3.5]
-set xtics ("64" 0, "128" 1, "256" 2, "512" 3, "1024" 4, "1500" 5, "2048" 6)
+set ytics 50
+set yrange [0:100]
+set xtics ("1k" 0, "4k" 1, "16k" 2, "64k" 3, "256k" 4)
 set xlabel "File size (bytes)"
-set xrange [-1:7]
+set xrange [-1:5]
 set terminal postscript eps lw 3 "Helvetica" 24
 set output "${OUTPUT}/out/nginx-wrk-latency.eps"
 
 plot \
-   '${OUTPUT}/nginx-docker.dat' usin (\$0-0.3):(\$1/1000):(\$2/1000) w boxerr fill pattern 2 lt 1 lc rgb "green"  title "docker(mac)" ,\
-   '${OUTPUT}/nginx-lkl.dat' usin (\$0-0):(\$1/1000):(\$2/1000) w boxerr fill pattern 4 lt 1 lc rgb "cyan" title "lkl", \
-   '${OUTPUT}/nginx-native.dat' usin (\$0+0.3):(\$1/1000):(\$2/1000) w boxerr fill pattern 0 lt 1 lc rgb "red" title "native(mac)" 
+   '${OUTPUT}/nginx-http-lkl-hijack.dat' usin (\$0-0.35):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_RUNU title "lkl-hijack", \
+   '${OUTPUT}/nginx-https-lkl-hijack.dat' usin (\$0-0.25):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_RUNU fill patter 1 title "lkl-hijack(https)", \
+   '${OUTPUT}/nginx-https-ktls-lkl-hijack.dat' usin (\$0-0.15):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_RUNU fill patter 1 title "lkl-hijack(https-ktls)", \
+   '${OUTPUT}/nginx-https-ktls-sendfile-lkl-hijack.dat' usin (\$0-0.05):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_RUNU fill patter 1 title "lkl-hijack(https-ktls-sendfile)", \
+   '${OUTPUT}/nginx-http-native.dat' usin (\$0+0.05):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_NATIVE title "native" ,\
+   '${OUTPUT}/nginx-https-native.dat' usin (\$0+0.15):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_NATIVE fill patter 1 title "native(https)", \
+   '${OUTPUT}/nginx-https-ktls-native.dat' usin (\$0+0.25):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_NATIVE fill patter 1 title "native(https-ktls)", \
+   '${OUTPUT}/nginx-https-ktls-sendfile-native.dat' usin (\$0+0.35):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_NATIVE fill patter 1 title "native(https-ktls-sendfile)"
 
 set terminal png lw 3 14 crop
 set output "${OUTPUT}/out/nginx-wrk-latency.png"
@@ -77,24 +124,22 @@ replot
 
 # combined
 set ylabel "Throughput (KReq/sec)"
-set ytics 5
-set yrange [0:20]
+set ytics 10
+set yrange [0:100]
 set y2label "Latency (msec)"
-set y2tics 5
+set y2tics 10
 set y2range [0:]
-set xtics ("64" 0, "128" 1, "256" 2, "512" 3, "1024" 4, "1500" 5, "2048" 6)
+set xtics ("1k" 0, "4k" 1, "16k" 2, "64k" 3, "256k" 4)
 set xlabel "File size (bytes)"
-set xrange [-1:7]
+set xrange [-1:5]
 set terminal postscript eps lw 3 "Helvetica" 24
 set output "${OUTPUT}/out/nginx-wrk-combined.eps"
 
 plot \
-   '${OUTPUT}/nginx-docker-thpt.dat' usi (\$0-0.3):(\$1/1000):(\$2/1000) w boxerr lt 1 lc rgb "green" fill pattern 2 title "docker(mac)" ,\
-   '${OUTPUT}/nginx-lkl-thpt.dat' usi (\$0-0):(\$1/1000):(\$2/1000) w boxerr lt 1 lc rgb "cyan" fill pattern 4 title "lkl", \
-   '${OUTPUT}/nginx-native-thpt.dat' usi (\$0+0.3):(\$1/1000):(\$2/1000) w boxerr lt 1 lc rgb "red" fill pattern 0 title "native(mac)" ,\
-   '${OUTPUT}/nginx-docker.dat' usin (\$0-0.3):(\$1/1000):(\$2/1000) w yerror ps 1 lc rgb "green" ax x1y2 notitle ,\
-   '${OUTPUT}/nginx-lkl.dat' usin (\$0-0):(\$1/1000):(\$2/1000) w yerror ps 1 lc rgb "cyan" ax x1y2 notitle, \
-   '${OUTPUT}/nginx-native.dat' usin (\$0+0.3):(\$1/1000):(\$2/1000) w yerror ps 1 lc rgb "red" ax x1y2 notitle
+   '${OUTPUT}/nginx-http-lkl-hijack-thpt.dat' usi (\$0-0):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_RUNU title "lkl", \
+   '${OUTPUT}/nginx-http-native-thpt.dat' usi (\$0+0.3):(\$1/1000):(\$2/1000) w boxerr $BOX_PAT_NATIVE title "native" ,\
+   '${OUTPUT}/nginx-http-lkl-hijack.dat' usin (\$0-0):(\$1/1000):(\$2/1000) w yerror ps 1 lc rgb "cyan" ax x1y2 notitle, \
+   '${OUTPUT}/nginx-http-native.dat' usin (\$0+0.3):(\$1/1000):(\$2/1000) w yerror ps 1 lc rgb "red" ax x1y2 notitle
 
 set terminal png lw 3 14 crop
 set xtics nomirror font ",14"
